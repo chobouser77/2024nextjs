5267c8cb4864c50bd5b69bc541569f0291901c49
chobouser77 <gl8310239@gmail.com>
2024-09-26 01:03:39 +0900

    work

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
-
+// git add .
+      // git commit -m "work"
+      // npm run dev  <- 실행
+      // http://localhost:3000/  : 결과물 실시간
 export default function Home() {
   return (
-    <div class="text-[red] bg-[gray] w-[full] h-[1000px] flex items-center justify-center">
+    <div className="text-[] bg-[gray] w-[full] h-[1000px] flex items-center justify-center">
       안녕!!!
       </div>
-      // 
+      
   );
 }
