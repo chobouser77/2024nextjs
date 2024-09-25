d0a421c5d56127a04af8a3d3146bdb6f9c91782b
chobouser77 <gl8310239@gmail.com>
2024-09-26 01:08:10 +0900

    work

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 // git add .
-      // git commit -m "work"
-      // npm run dev  <- 실행
-      // http://localhost:3000/  : 결과물 실시간
+// git commit -m "work"
+// git push orgin main
+// npm run dev  <- 실행
+// http://localhost:3000/  : 결과물 실시간
 export default function Home() {
   return (
-    <div className="text-[] bg-[gray] w-[full] h-[1000px] flex items-center justify-center">
+    <div className="text-[blue] bg-[gray] w-[full] h-[1000px] flex items-center justify-center">
       안녕!!!
       </div>
       
